04951c6c91e59bb25774174793ab2bc132a3a9e3
xuehongye <2897451814@qq.com>
2017-07-07 09:33:17 +0800

    df

---
+++ mvc/static/js/xhylogin.js
@@ -0,0 +1,26 @@
+var reg = /^[a-zA-Z]{5,8}$/;
+var reg1 = /^[a-zA-Z0-9]{5,8}$/;
+$("input[name=uname]").blur(function () {
+    var val1 = $("input[name=uname]").val();
+    if (reg.test(val1)) {
+        $(".nametrue").css("display", "block");
+        $(".namefalse").css("display", 'none');
+    } else {
+        $(".namefalse").css("display", 'block');
+        $(".nametrue").css("display", "none");
+    }
+})
+$("input[name=upass]").blur(function () {
+    var val2 = $("input[name=upass]").val();
+    if (reg1.test(val2)) {
+        $(".passtrue").css("display", "block");
+        $(".passfalse").css("display", 'none');
+    } else {
+        $(".passfalse").css("display", 'block');
+        $(".passtrue").css("display", "none");
+    }
+})
+$(".login-btn").click(function () {
+    $(".login-btn").css("border", "2px solid #fd7846");
+    $(".login-btn>input").css("background", "#fd7846");
+})
---
+++ mvc/static/js/xhysplash.js
@@ -0,0 +1,1 @@
+$(".start").addClass('animated tada');
